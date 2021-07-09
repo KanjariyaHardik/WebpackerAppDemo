41d02226963a111a4f18b9efd7cfcf8af520bb80
Hardik Kanjariya <hardikgkanjariya@gmail.com>
2021-07-09 18:53:30 +0530

    Initial Rails 6 Setup

---
+++ app/javascript/packs/test.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  console.log("JS Initiated!")
+  console.log("Chunk First", _.chunk(['a', 'b', 'c', 'd'], 2))
+  console.log("Chunk Second", _.chunk(['a', 'b', 'c', 'd'], 3))
+});
